bc6931f62ed3be27610de78d0a80e4fa57c6505b
Thomas Ebner <t.ebner@student.tugraz.at>
2011-05-12 18:23:15 +0200

    fixed bugs

--- 02/implementation/feedforwardNN.m
+++ 02/implementation/feedforwardNN.m
@@ -1,5 +1,7 @@
 function [output, z, a1, a2] = feedforwardNN(w, x)
 
+ sigma = @(x) 1./(1+exp(-x));
+
 x_size = size(x);
 
 W1 = reshape(w(1:12), 4,3)';
@@ -8,8 +10,8 @@ W2 = reshape(w(13:17), 1,5)';
 
 a1 = [x ones(x_size(1),1)]*W1;
 
-z = 1./(1+exp(-a1));
+z = sigma(a1);
 
 a2 = [z ones(x_size(1),1)]*W2;
 
-output = 1/(1+exp(-a2));
+output = sigma(a2);
