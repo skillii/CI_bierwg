e4c1e72b015bda96687cd7a1f1dfa27906d6d014
Thomas Ebner <t.ebner@student.tugraz.at>
2011-06-24 01:12:04 +0200

    blabla

--- 05/implementation/ue5_1.m
+++ 05/implementation/ue5_1.m
@@ -14,6 +14,8 @@ plot(NLOS_train(1,:), NLOS_train(2,:), 'b o');
 
 legend('LOS', 'NLOS');
 title('Scatter Plot of training data');
+xlabel('x1');
+ylabel('x2');
 
 %%
 
@@ -26,6 +28,7 @@ x1LOS = LOS_train(1,:);
 
 
 x_ray = linspace(-1, 5, 50);
+ray_pdf_func = @(x) x ./ (ray_sigma.^2) .* exp(-x.^2./(2*ray_sigma.^2));
 ray_pdf = x_ray ./ (ray_sigma.^2) .* exp(-x_ray.^2./(2*ray_sigma.^2));
 
 
@@ -36,12 +39,93 @@ ray_pdf = x_ray ./ (ray_sigma.^2) .* exp(-x_ray.^2./(2*ray_sigma.^2));
 [Nrice,Xrice] = hist(LOS_train(1,:), 100);
 [Nray,Xray] = hist(NLOS_train(1,:), 100);
 
-%Nrice = Nrice ./ (sum(Nrice)*(Xrice(end)-Xrice(1)));
-%Nray = Nray ./ (sum(Nray)*(Xray(end)-Xray(1)));
+Nrice = Nrice ./ (sum(Nrice)*(Xrice(end)-Xrice(1)))*100;
+Nray = Nray ./ (sum(Nray)*(Xray(end)-Xray(1)))*100;
+
+
 
 figure;
 bar(Xrice, Nrice, 'r');
 hold on;
 bar(Xray, Nray, 'b');
 
+plot(x_rice, rice_pdf, 'r- ', 'LineWidth', 3);
+plot(x_ray, ray_pdf, 'b- ', 'LineWidth', 3);
+
+xlim([0 5]);
+ylim([0 1]);
+
+legend('LOS', 'NLOS');
+title('Estimation of Rayleigh and Rice PDF');
+
 %%
+
+% 4.)
+
+LOSPrior = length(LOS_train(1,:)) / (length(LOS_train(1,:)) + length(NLOS_train(1,:)));
+NLOSPrior = length(NLOS_train(1,:)) / (length(LOS_train(1,:)) + length(NLOS_train(1,:)));
+
+%ML Classification:
+
+% first we try to classify the LOS_test data
+pNLOS = ray_pdf_func(LOS_test(1,:));
+pLOS = interp1(x_rice, rice_pdf, LOS_test(1,:));
+
+figure;
+plot(LOS_test(1,pLOS>pNLOS), LOS_test(2,pLOS>pNLOS), 'r o');
+hold on;
+plot(LOS_test(1,pLOS<=pNLOS), LOS_test(2,pLOS<=pNLOS), 'b o');
+
+
+correct_classified = sum(pLOS>pNLOS);
+
+%----
+% now we try to classify the NLOS_test data
+pNLOS = ray_pdf_func(NLOS_test(1,:));
+pLOS = interp1(x_rice, rice_pdf, NLOS_test(1,:));
+
+%figure;
+plot(NLOS_test(1,pLOS>pNLOS), NLOS_test(2,pLOS>pNLOS), 'r o');
+plot(NLOS_test(1,pLOS<=pNLOS), NLOS_test(2,pLOS<=pNLOS), 'b o');
+
+
+legend('as LOS classified', 'as NLOS classified');
+title('ML-Classification: Scatter Plot of test data');
+xlabel('x1');
+ylabel('x2');
+
+correct_classified = correct_classified + sum(pLOS<=pNLOS);
+
+disp(['ML: Es wurden ', num2str(correct_classified/(length(LOS_train(1,:)) + length(NLOS_train(1,:)))*100), '% korrekt klassifiziert']);
+%%
+%Bayes Classification---------------------------------------------
+% first we try to classify the LOS_test data
+
+pNLOS = ray_pdf_func(LOS_test(1,:)) * NLOSPrior;
+pLOS = interp1(x_rice, rice_pdf, LOS_test(1,:))*LOSPrior;
+
+figure;
+plot(LOS_test(1,pLOS>pNLOS), LOS_test(2,pLOS>pNLOS), 'r o');
+hold on;
+plot(LOS_test(1,pLOS<=pNLOS), LOS_test(2,pLOS<=pNLOS), 'b o');
+
+
+correct_classified = sum(pLOS>pNLOS);
+%----
+% now we try to classify the NLOS_test data
+pNLOS = ray_pdf_func(NLOS_test(1,:)) * NLOSPrior;
+pLOS = interp1(x_rice, rice_pdf, NLOS_test(1,:))*LOSPrior;
+
+%figure;
+plot(NLOS_test(1,pLOS>pNLOS), NLOS_test(2,pLOS>pNLOS), 'r o');
+plot(NLOS_test(1,pLOS<=pNLOS), NLOS_test(2,pLOS<=pNLOS), 'b o');
+
+
+legend('as LOS classified', 'as NLOS classified');
+title('Bayes-Classification: Scatter Plot of test data');
+xlabel('x1');
+ylabel('x2');
+
+correct_classified = correct_classified + sum(pLOS<=pNLOS);
+disp(['Bayes: Es wurden ', num2str(correct_classified/(length(LOS_train(1,:)) + length(NLOS_train(1,:)))*100), '% korrekt klassifiziert']);
+
