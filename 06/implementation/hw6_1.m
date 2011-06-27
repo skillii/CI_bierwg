9f566279cb200fe8aac642d2c53625f40f4074e2
Stefan Nöhmer <s.noehmer@gmail.com>
2011-06-27 22:58:31 +0200

    first version of 6.1

---
+++ 06/implementation/hw6_1.m
@@ -0,0 +1,51 @@
+% Homework 6: Expectation Maximation
+
+%clc;
+%clear all;
+%close all;
+
+
+dataset = load('vowels.mat');
+
+M = 5;
+alpha_0 = [1; 1; 1; 1; 1];
+mu_0 = [0.4, 1; 0.5, 1.2; 0.6, 1.4; 0.7, 1.6; 0.8, 1.8];
+Sigma_0 = cat(3, [1, 0; 0, 1], [1, 0; 0, 1], [1, 0; 0, 1], [1, 0; 0, 1], [1, 0; 0, 1]);
+max_iter = 1000;
+
+
+% generate scatter plots
+
+% figure;
+% plot(dataset.a(:,1), dataset.a(:,2), 'b.');
+% hold on;
+% plot(dataset.e(:,1), dataset.e(:,2), 'g.');
+% plot(dataset.i(:,1), dataset.i(:,2), 'r.');
+% plot(dataset.o(:,1), dataset.o(:,2), 'k.');
+% plot(dataset.u(:,1), dataset.u(:,2), 'c.');
+% plot(dataset.y(:,1), dataset.y(:,2), 'm.');
+% xlabel('x1');
+% ylabel('x2');
+% title('Scatter Plot of Labeled Input Data');
+% legend('A', 'E', 'I', 'O', 'U', 'Y');
+
+
+% run EM algorithm
+
+[alpha, mu, Sigma, L] = EM(dataset.allvow, M, alpha_0, mu_0, Sigma_0, max_iter);
+
+
+% plot GMM
+
+for m = 1:M
+    %plotGaussContour(mu(m,:), Sigma(:,:,m));
+end
+
+
+% plot log-Likelihood
+
+% figure;
+% plot(L);
+% xlabel('Iteration');
+% ylabel('log-Likelihood');
+% title('log-Likelihood Behaviour');
